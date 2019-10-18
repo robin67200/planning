a16b71c7e63ec90e435f3b93af06b50e7e12c3c6
robin67200 <48128624+robin67200@users.noreply.github.com>
2019-10-18 08:33:47 +0200

    annees

--- Planning-SPA/src/app/view/annee/annee-list/annee-list.component.ts
+++ Planning-SPA/src/app/view/annee/annee-list/annee-list.component.ts
@@ -2,6 +2,8 @@ import { Annee } from './../models/annee';
 import { AnneeService } from './../services/annee.service';
 import { Component, OnInit } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap';
+import { SimpleModalService } from 'ngx-simple-modal';
+import { ModalSimpleInputComponent } from 'src/app/components/modals/simple-input-modals';
 
 @Component({
   selector: 'app-annee-list',
@@ -10,13 +12,14 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap';
 })
 export class AnneeListComponent implements OnInit {
 
+  anneess: Annee[] = [];
   annees: any;
   bsModalRef: BsModalRef;
-  annne: Annee;
+  annee: Annee;
 
   constructor(
     private service: AnneeService,
-    private modalService: BsModalService,
+    private modals: SimpleModalService,
     ) { }
 
   ngOnInit() {
@@ -38,4 +41,36 @@ export class AnneeListComponent implements OnInit {
     this.bsModalRef.content.closeBtnName = 'Close';
   }
 }*/
+
+ addAnnee() {
+    this.modals.addModal(ModalSimpleInputComponent, {
+      title: `Ajout d'une nouvelle année`,
+      message: 'Veuillez entrer son nom',
+      defaultValue: '',
+      label: 'Nom'
+    }).subscribe(result => {
+      if (result) {
+        const annee = new Annee('');
+        this.service.postAnnee(annee).subscribe(res => {
+          this.anneess.push(res);
+        });
+      }
+    });
+  }
+
+   modifyAnnee(annee: Annee) {
+    this.modals.addModal(ModalSimpleInputComponent, {
+      title: 'Modification de la matière',
+      message: 'Veuillez entrer le nom de la matière',
+      defaultValue: annee.nom,
+      label: 'Nom'
+    }).subscribe(result => {
+      if (result) {
+        annee.nom = result.toString();
+        this.service.putAnnee(annee.id, annee).subscribe(res => {
+          this.ngOnInit();
+        });
+      }
+    });
+  }
 }
