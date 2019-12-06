da292fb5b4de26bb16052d13a0bb1c290f670773
jejeOtt <joottjeremy3@gmail.com>
2019-12-06 10:45:27 +0100

    message

--- Planning-SPA/src/app/view/nav/nav.module.ts
+++ Planning-SPA/src/app/view/nav/nav.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule, MatListModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [NavComponent],
@@ -17,6 +18,8 @@ import { RouterModule } from '@angular/router';
       MatListModule,
       FormsModule,
       ReactiveFormsModule,
+      MatTooltipModule,
+
     ],
   exports: [NavComponent]
 })
